57d3b8e9b42c9bb47da9be3a531c2b8ef141966a
Grant Janecek <g.a.koehler@gmail.com>
2021-01-16 08:50:54 -0500

    first commit

---
+++ source/index.blade.php
@@ -0,0 +1,14 @@
+@extends('_layouts.master')
+
+@section('body')
+    <div class="min-h-screen bg-gradient-to-br from-indigo-100 to-white flex justify-center items-center">
+        <div>
+            <h1 class="text-6xl font-bold">Grant Janecek</h1>
+            <div class="flex space-x-3 justify-center align-baseline">
+                <a href="https://github.com/grantjanecek">Github</a>
+                <a href="https://www.linkedin.com/in/grant-janecek-20170752/">Linkedin</a>
+                <a href=""></a>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ source/nav.blade.php
@@ -0,0 +1,7 @@
+<!-- <div class="w-full p-6 flex justify-between">
+    <div>Image</div>
+    <div class="flex space-x-2">
+        <a href="#about">About</a>
+        <a href="#blog">Blog</a>
+    </div>
+</div> -->
