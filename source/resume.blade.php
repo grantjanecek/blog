f03059a6b98d61316a83c0df5513da97f7b45c9b
Grant <g.a.koehler@gmail.com>
2021-01-29 19:20:05 -0500

    Moved  skills to top

--- source/resume.blade.php
+++ source/resume.blade.php
@@ -5,11 +5,11 @@
         <div class="container w-1/2 mx-auto shadow bg-white p-10 space-y-4">
             @include('_resume.header')
             <hr class="border-b my-6">
+            @include('_resume.skills')
+            <hr class="border-b my-6">
             @include('_resume.experience')
             <hr class="border-b my-6">
             @include('_resume.education')
-            <hr class="border-b my-6">
-            @include('_resume.skills')
         </div>
     </div>
 @endsection
